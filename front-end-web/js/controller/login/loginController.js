8197c42850341c376352d436de2ff1daa397b6de
romulo-xavier25 <romulo.xavier25@gmail.com>
2018-03-04 14:52:14 -0300

    adicionando validação nos campos na tela login

--- front-end-web/js/controller/login/loginController.js
+++ front-end-web/js/controller/login/loginController.js
@@ -5,19 +5,25 @@ app.controller('loginController', function ($scope, $http, $route, $routeParams,
 		logarDAO.login = login;
 		logarDAO.senha = senha;
 
-		var logar = angular.toJson(logarDAO);
-        $http.post('http://localhost/pointstorePA3/index.php/login', logar)
-        .success(function(retorno){
-            if (retorno != undefined && retorno != null && retorno != "") {
-            	localStorage.setItem("usuario", JSON.stringify(retorno));
-            	window.location.href = 'home.html';
-            }else{
-            	alert("Usuário ou senha inválidos!");
-            }
+		
 
-        }).error(function(){
-            $scope.erroMessage = "Usuario "+logarDAO.login+" não foi logado!";
-        });
+            if(document.formLogin.login.value == "" || document.formLogin.senha.value == ""){
+                alert("preencha os campos que estiverem vazio!");
+            }else{
+                var logar = angular.toJson(logarDAO);
+                $http.post('http://localhost/pointstorePA3/index.php/login', logar)
+                .success(function(retorno){
+                    if(retorno != undefined && retorno != null && retorno != "") {
+                        localStorage.setItem("usuario", JSON.stringify(retorno));
+                        window.location.href = 'home.html';
+                    }else{
+                        alert("Usuário ou senha inválidos!");
+                    }
+                }).error(function(){
+                    alert("Usuário ou senha inválidos!");
+                });
+            }
+                
 
 			
 	}
