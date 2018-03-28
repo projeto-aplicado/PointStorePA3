53a95fef5132387b15b0ff2588e279a561d6022c
romulo-xavier25 <romulo.xavier25@gmail.com>
2018-03-28 11:47:57 -0300

    funcionalidade cadastrar venda do front-end web funcionando

--- front-end-web/js/controller/sistema/vendasController.js
+++ front-end-web/js/controller/sistema/vendasController.js
@@ -31,6 +31,37 @@ app.controller('vendasController', function ($scope, $http, $route, $routeParams
                 return false;
             }
 
+            $http.post('http://localhost/pointstorePA3/index.php/venda', venda)
+                .success(function (retorno) {
+                    alert(retorno.mensagem);
+                }).error(function () {
+                    alert("Venda não foi salva!");
+            });
         }
 
-});
+    $scope.cadastrarTiposDePontos = function(){
+        var meusPontosDAO = new Object();
+        meusPontosDAO.tipoDePontos = $scope.pontos.tipoDePontos;
+        meusPontosDAO.quantidadePonto = $scope.pontos.quantidadePonto;
+        meusPontosDAO.id = $scope.pontos.id;
+        var tipoDePontosUsuario = angular.toJson(meusPontosDAO);
+
+        if(!$scope.pontos.tipoDePontos){
+            alert('Por favor, escolha o tipo de ponto!');
+            return false;
+        }
+
+        if(!$scope.pontos.numeroCartao){
+            alert('Por favor, digite a quantidade de ponto');
+            return false;
+        }
+
+        $http.put('http://localhost/pointstorePA3/index.php/meuspontos/atualizar', tipoDePontosUsuario)
+            .success(function(retorno){
+                alert("ponto cadastrado com sucesso!");
+            }).error(function(){
+            alert("erro no cadastro do ponto");
+        });
+    }
+    }
+);
